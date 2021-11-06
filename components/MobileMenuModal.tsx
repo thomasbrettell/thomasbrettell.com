b599ce37754a75e35efbc09a2ce1804d49559dff
thomasbrettell <thomas.a.brettell@gmail.com>
2021-11-06 12:52:39 +1100

    Fix: mobile menu not using next/link

--- components/MobileMenuModal.tsx
+++ components/MobileMenuModal.tsx
@@ -1,18 +1,21 @@
-import {FC, Fragment} from 'react';
-import {Modal, Link, Spacer} from '@geist-ui/react';
-import {pages} from '../constants';
+import { FC, Fragment } from 'react';
+import { Modal, Link, Spacer } from '@geist-ui/react';
+import { pages } from '../constants';
+import NextLink from 'next/link';
 
 interface MenuInterface {
   state: boolean;
   closeHandler: () => void;
 }
 
-const MobileMenuModal: FC<MenuInterface> = ({state, closeHandler}) => {
+const MobileMenuModal: FC<MenuInterface> = ({ state, closeHandler }) => {
   return (
     <Modal visible={state} onClose={closeHandler}>
       {pages.map((page, index) => (
         <Fragment key={page.name}>
-          <Link href={page.link}>{page.name}</Link>
+          <NextLink href={page.link}>
+            <Link>{page.name}</Link>
+          </NextLink>
           {index + 1 !== pages.length && <Spacer h={1} />}
         </Fragment>
       ))}
